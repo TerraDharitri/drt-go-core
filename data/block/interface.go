3b1d4df4b8671cfebeab289bacf099967a78571d
kasiaurangabad <kasi.aurangabad@gmail.com>
2024-12-30 22:25:20 +0530

    v0.0.1

---
+++ data/block/interface.go
@@ -0,0 +1,9 @@
+package block
+
+import "github.com/TerraDharitri/drt-go-core/data"
+
+// EmptyBlockCreator is able to create empty block instances
+type EmptyBlockCreator interface {
+	CreateNewHeader() data.HeaderHandler
+	IsInterfaceNil() bool
+}
